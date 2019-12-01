40553bae03aa8b88bc2ede918c0aa8b8b0740982
Jeff Rudd <jeffrudd19@gmail.com>
2019-12-01 14:30:47 -0500

    Full background image compressed

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
 let t1 = gsap.timeline();
 
-t1.to(".text1", {delay: 1.5, duration: 0.5, opacity: 1})
+t1.to(".text1", {delay: 1, duration: 0.5, opacity: 1})
     .to(".text1", {delay: 3.5, duration: 0.5, opacity: 0})
 
     .add("fade")
     .to(".frame1", {duration: 1, opacity:0}, "fade")
-    // .set(".frame1", {opacity: 0})
     .set(".full", {yPercent: -38, xPercent:-15, scale: 0.75}, "fade")
     .to(".full, .layer", {opacity: 1, duration: 1}, "fade")
 
-    // .set(".layer", {opacity: 1})
     .to(".text2", {duration: 0.5, opacity: 1})
 
     .add("motion")
@@ -24,5 +22,17 @@ t1.to(".text1", {delay: 1.5, duration: 0.5, opacity: 1})
     .fromTo(".title4", {marginLeft:-800, opacity:0}, {marginLeft:0, opacity:1})
     .fromTo(".title4", {width:0}, {width:255})
     .to(".text4", {delay: 1, opacity:1})
-    // .fromTo(".button4", {rotation: -20, opacity:0}, {opacity: 1, rotation: 0, ease: "steps(12)"})
-    .to(".button4",{opacity: 1})
+    .to(".button4",{delay: 1, opacity: 1})
+
+    $(".gallery").hover(function() {
+        t1.to(".button4",
+            {webkitFilter: "brightness(1.3)",
+              filter: "brightness(1.3)"})
+    });
+    
+
+    $(".gallery").mouseleave(function() {
+        t1.to(".button4", 
+            {webkitFilter: "brightness(1)", 
+            filter: "brightness(1)"})
+    });
