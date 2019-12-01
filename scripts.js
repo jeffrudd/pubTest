eab0d711c84f65c8004fc25b302e2dc2f821f5c2
Jeff Rudd <jeffrudd19@gmail.com>
2019-12-01 00:11:02 -0500

    Added large background panel for panels 2,3,4

--- scripts.js
+++ scripts.js
@@ -1,30 +1,22 @@
 let t1 = gsap.timeline();
 
-t1.to(".frame1, .text1", {delay: 1, duration: 2.5, opacity:0})
-    // .to(".text1", {delay: 1, duration: 2.5, opacity:0})
+t1.to(".text1", {delay: 1.5, duration: 0.5, opacity: 1})
+    .to(".text1", {delay: 3.5, duration: 0.5, opacity: 0})
+    .set(".frame1", {opacity: 0})
 
-    .to(".frame2, .text2", {duration: 2.5, opacity:1})
-    .to(".text3", {duration: 2.5, opacity:1})
+    .set(".full", {yPercent: -20, xPercent: 20, scale: 1.5, opacity: 1})
+    .to(".text2", {duration: 0.5, opacity: 1})
 
-    .to(".frame2", {xPercent:-100, duration: 2.5}, 5)
-    .to(".text2", {opacity: 0, duration: 2}, 5)
+    .add("motion")
+    .to(".full", {delay: 2, xPercent:-50, duration: 2.5}, "motion")
+    .to(".text2", {delay: 1.5, duration: 1, opacity: 0}, "motion")
+    .to(".text3", {delay: 4.5, duration: 0.5, opacity: 1}, "motion")
 
-    .set(".frame3",{xPercent:100, opacity:1}, 5)
-    // .set(".text3",{xPercent:100, opacity:1}, 5)
-
-    .to(".frame3", {xPercent:0, duration: 2.5}, 5)
-    // .to(".text3", {xPercent:0, duration: 2.5}, 5)
-
-    .set(".frame2", {opacity: 0})
-    // .set(".text2", {opacity: 0})
     
-    .to(".frame3", {duration: 0.5, opacity:0})
-    .to(".text3", {duration: 0, opacity:0})
-
-    .set(".frame4", {scale: 2})
-    .to(".frame4", {scale: 1, duration: 1, opacity: 1})
+    .to(".text3", {delay: 3, duration: 0, opacity:0})
+    .to(".full", {scale: 0.75, xPercent:-20, duration: 0.5, opacity: 1})
 
     .fromTo(".title4", {marginLeft:-800, opacity:0}, {marginLeft:0, opacity:1})
     .fromTo(".title4", {width:0}, {width:255})
-    .to(".text4", {opacity:1})
-    .fromTo(".button4", {opacity:0, rotation: -20}, {opacity: 1, rotation: 0, ease: "steps(12)"})
+    .to(".text4", {delay: 1, opacity:1})
+    .fromTo(".button4", {opacity: 0, rotation: -20}, {delay: 1, opacity: 1, rotation: 0, ease: "steps(12)"})
